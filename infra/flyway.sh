577a05daad024b0781acf81c69e437311798f59f
Rodrigo <74364367+rodrigocruzskrowz@users.noreply.github.com>
2026-04-17 14:26:48 +0100

    Delete infra/flyway.sh

--- infra/flyway.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker run --rm \
-  --network ownfinance-ai_default \
-  -v "$(pwd)/backend/src/main/resources/db/migration:/flyway/sql" \
-  flyway/flyway:10 \
-  -url="jdbc:postgresql://postgres:5432/ownfinance" \
-  -user="ownfinance_user" \
-  -password="0wnfinanc€" \
-  migrate
